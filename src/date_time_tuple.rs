183391f01c46a098c864cc423d7883b637862f52
samueldple <samueldple@gmail.com>
2018-10-24 17:37:37 +0100

    Implement PartialOrd and Ord for DateTimeTuple

--- src/date_time_tuple.rs
+++ src/date_time_tuple.rs
@@ -1,4 +1,5 @@
 use date_tuple::DateTuple;
+use std::cmp::Ordering;
 use std::fmt;
 use time_tuple::TimeTuple;
 
@@ -29,6 +30,26 @@ impl DateTimeTuple {
 impl fmt::Display for DateTimeTuple {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}@{}", self.d.to_string(), self.t.to_string())
+    }
+}
+
+impl PartialOrd for DateTimeTuple {
+    fn partial_cmp(&self, other: &DateTimeTuple) -> Option<Ordering> {
+        if self.d == other.d {
+            self.t.partial_cmp(&other.t)
+        } else {
+            self.d.partial_cmp(&other.d)
+        }
+    }
+}
+
+impl Ord for DateTimeTuple {
+    fn cmp(&self, other: &DateTimeTuple) -> Ordering {
+        if self.d == other.d {
+            self.t.cmp(&other.t)
+        } else {
+            self.d.cmp(&other.d)
+        }
     }
 }
 
@@ -67,6 +88,24 @@ mod tests {
             ::time_tuple::TimeTuple::new(8, 30, 0),
         );
         assert_eq!(tuple1, tuple2);
+    }
+
+    #[test]
+    fn test_comparisons() {
+        let tuple1 = super::DateTimeTuple::new(
+            ::date_tuple::DateTuple::new(2000, 5, 10).unwrap(),
+            ::time_tuple::TimeTuple::new(8, 30, 0),
+        );
+        let tuple2 = super::DateTimeTuple::new(
+            ::date_tuple::DateTuple::new(2000, 5, 10).unwrap(),
+            ::time_tuple::TimeTuple::new(9, 30, 0),
+        );
+        let tuple3 = super::DateTimeTuple::new(
+            ::date_tuple::DateTuple::new(2000, 5, 11).unwrap(),
+            ::time_tuple::TimeTuple::new(8, 30, 0),
+        );
+        assert!(tuple1 < tuple2);
+        assert!(tuple2 < tuple3);
     }
 
 }
