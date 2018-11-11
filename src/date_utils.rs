2a501d897b7828e1ae649fbf9cfa1ad1b297e5e8
samueldple <samueldple@gmail.com>
2018-11-11 15:59:37 +0000

    Add function for returning current date as DateTuple

--- src/date_utils.rs
+++ src/date_utils.rs
@@ -1,13 +1,31 @@
+use date_tuple::DateTuple;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+const UNIX_EPOCH_START_YEAR: u16 = 1970;
 
 /// Takes a year as a u16 and returns whether it is a leap year.
 pub fn is_leap_year(year: u16) -> bool {
     (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 }
 
+/// Gets the current date as a `DateTuple`
+pub fn now_as_datetuple() -> DateTuple {
+    let mut seconds = duration_since_unix_epoch().as_secs();
+    let years = seconds / 31557600;
+    seconds -= years * 31557600;
+    let months = seconds / 2629800;
+    seconds -= months * 2629800;
+    let days = seconds / 86400 + 1; //Days past plus current
+    DateTuple::new(
+        years as u16 + UNIX_EPOCH_START_YEAR,
+        months as u8,
+        days as u8,
+    ).unwrap()
+}
+
 /// Gets a duration using `std::time::SystemTime::now()` since the
 /// unix epoch.
-pub fn duration_since_unix_epoch() -> Duration {
+fn duration_since_unix_epoch() -> Duration {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap_or_else(|_| Duration::new(0, 0))
